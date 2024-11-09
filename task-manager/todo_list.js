df07a01e194adc6ee3104b26a4c528a513b81b78
qs-ashish05 <aashishjha8080@gmail.com>
2024-11-09 11:32:56 +0530

    project 3 to do

---
+++ task-manager/todo_list.js
@@ -0,0 +1,60 @@
+
+const taskInput = document.getElementById("taskInput");
+
+const addTaskBtn = document.getElementById("addTaskBtn");
+const taskList = document.getElementById("taskList");
+const clearCompletedBtn = document.getElementById("clearCompletedBtn");
+
+let tasks = [];
+
+function addTask() {
+
+    const taskText = taskInput.value.trim();
+    if (taskText !== "") {
+        tasks.push({ text: taskText});
+        taskInput.value = "";
+        displayTasks();
+    }
+}
+
+
+function displayTasks() {
+    // console.log(navigator.appName);
+    // console.log(navigator.appVersion);
+    taskList.innerHTML = "";
+    tasks.forEach((task, index) => {
+        const li = document.createElement("li");
+        li.innerHTML = `<input type="checkbox" id="task-${index}" ${task.completed ? "checked" : ""}>
+            <label for="task-${index}">${task.text}</label>`;
+        li.querySelector("input").addEventListener("change", () => toggleTask(index));
+        taskList.appendChild(li);
+    });
+}
+
+function toggleTask(index) {
+    tasks[index].completed = !tasks[index].completed;
+    displayTasks();
+}
+
+
+function clearCompletedTasks() {
+    tasks = tasks.filter(task => !task.completed);
+    displayTasks();
+}
+
+// function isLocalStorageSupported() {
+//     try {
+//         const testKey = '__test__';
+//         localStorage.setItem(testKey, testKey);
+//         localStorage.removeItem(testKey);
+//         return true;
+//     } catch (e) {
+//         return false;
+//     }
+// }
+
+addTaskBtn.addEventListener("click", addTask);
+clearCompletedBtn.addEventListener("click", clearCompletedTasks);
+
+
+displayTasks();
